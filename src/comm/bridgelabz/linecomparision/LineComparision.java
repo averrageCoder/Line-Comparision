d3cbee9f4bbe8b205e67b004827c1f47e8f4fa17
Harsh R Jain <rjainharsh@gmail.com>
2021-08-20 18:05:14 +0530

    ADDED: uc1 - length calculation

--- src/comm/bridgelabz/linecomparision/LineComparision.java
+++ src/comm/bridgelabz/linecomparision/LineComparision.java
@@ -4,8 +4,13 @@ public class LineComparision {
 
 	public static void main(String[] args) {
 		
-		System.out.println("<=== Welcome to Line Comparison Computation Program ==>");
-
+		double x1=0,x2=2,y1=2,y2=4;
+		double length;
+		
+		length=Math.sqrt(Math.pow((x2-x1),2)+ Math.pow((y2-y1),2));
+		
+		System.out.println("Length of line :"+ length);
+		
 	}
 
 }
