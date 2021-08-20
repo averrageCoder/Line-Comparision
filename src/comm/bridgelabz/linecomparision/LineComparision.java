8bb246e9c56860fb6759ea620e188676d221bd37
Harsh R Jain <rjainharsh@gmail.com>
2021-08-20 17:52:56 +0530

    ADDED: welcome messsage

---
+++ src/comm/bridgelabz/linecomparision/LineComparision.java
@@ -0,0 +1,11 @@
+package comm.bridgelabz.linecomparision;
+
+public class LineComparision {
+
+	public static void main(String[] args) {
+		
+		System.out.println("<=== Welcome to Line Comparison Computation Program ==>");
+
+	}
+
+}
