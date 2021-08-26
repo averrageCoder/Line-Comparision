68c5ffd69227c2e2340838c7f7552cfbbc465d6b
Harsh R Jain <rjainharsh@gmail.com>
2021-08-26 18:58:08 +0530

    REFACTORED: Line, Point & LineComparator class added. LineComparatorIF also added

---
+++ src/comm/bridgelabz/linecomparision/Point.java
@@ -0,0 +1,20 @@
+package comm.bridgelabz.linecomparision;
+
+public class Point {
+
+	public int xCoordinate, yCoordinate;
+	
+	public Point(int x, int y) {
+		this.xCoordinate = x;
+		this.yCoordinate = y;
+	}
+
+	public int getYCoordinate() {
+		return this.xCoordinate;
+	}
+
+	public int getXCoordinate() {
+		return this.yCoordinate;
+	}
+	
+}
